310e97088da56efe2cafac74b4660269207e8226
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-07-28 18:36:50 +0300

    Initial Commit

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    # Import the variable 'a' from variable_load_2.py
+    from variable_load_2 import a
+
+    # Print the value of 'a'
+    print(a)
